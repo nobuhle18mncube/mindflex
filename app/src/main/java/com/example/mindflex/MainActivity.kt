64b389045f67de486ae3c2a3f389152650246e96
Nobuhle Gloria Mncube <ST10215684@rcconnect.edu.za>
2025-11-05 13:06:48 +0200

    Test

--- app/src/main/java/com/example/mindflex/MainActivity.kt
+++ app/src/main/java/com/example/mindflex/MainActivity.kt
@@ -14,7 +14,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
